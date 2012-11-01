15c1c9a0f293d730f33b2171c16db0d2f33ac268
zmulony <sefferynka@ubuntu.(none)>
2012-11-01 18:41:21 +0100

    truly hoping this time the app will be successfully deployed on heroku - changed db to postgresql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstSPA::Application.config.secret_token = '8f6ce09b32d61ccb582413fb3124d0c0a4af95fedc7eccf1f91a91fced12b33458cadd46a9f7244548d3e5f77146a6ae9d2c35619c2302dbb6d21df14f365a08'
+FirstSPA::Application.config.secret_token = 'a3adb1d8ba09e7a9d74033035250cb05f09255588e603f8da8be544e52f87a8289999f286adb4e35d9f543a2527e4239b188ed0749f0a67b4623b2817be62939'
