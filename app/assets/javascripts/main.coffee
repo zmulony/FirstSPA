5ba08498e29c83858e3adb16ef818e73aa0d541a
zmulony <sefferynka@ubuntu.(none)>
2012-11-01 17:54:48 +0100

    hello world in spa

---
+++ app/assets/javascripts/main.coffee
@@ -0,0 +1,5 @@
+class Main
+	constructor: ->
+		alert("Hello world!")
+
+new Main()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstSPA::Application.config.secret_token = 'b1e9c770861234f4ee78629f29c8cf38c52ab12bc7bbad89f7e0276c47847ed3a263bd7ed4d4b2a0e865d11d8b4e4bd046b34f8fa2a2c7a2ba8fa7ec11cad56c'
